1361ea6b8875f2634ac12c12db5196631d15d51f
datoprak <106121887+datoprak@users.noreply.github.com>
2023-10-12 00:46:39 +0300

    Create footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
 import { GithubFilled } from "@ant-design/icons";
 import { Typography } from "antd";
+import "../styles/Footer.css";
 
 const Footer = () => {
   return (
     <footer>
       <Typography.Link href="https://github.com/datoprak">
-        <GithubFilled />
-        Github
+        <div className="link-container">
+          <GithubFilled />
+          <span>Github</span>
+        </div>
       </Typography.Link>
     </footer>
   );
